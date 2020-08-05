2e00d7b750e0400d33df5b5b58466dbc97ac5a3e
jykim74 <jykim74@gmail.com>
2020-08-05 11:13:28 +0900

    Base work

--- ca_reg_srv/reg_srv.c
+++ ca_reg_srv/reg_srv.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
 
 #include "js_pki.h"
 #include "js_http.h"
 #include "js_process.h"
 #include "js_db.h"
+#include "js_cfg.h"
 
 #include "reg_srv.h"
 
-const char* g_dbPath = "D:/data/ca.db";
+const char* g_dbPath = NULL;
+static char g_sConfigPath[1024];
+static char g_sBuildInfo[1024];
+
+int g_nVerbose = 0;
+JEnvList    *g_pEnvList = NULL;
+
+const char *getBuildInfo()
+{
+    sprintf( g_sBuildInfo, "Version: %s Build Date : %s %s",
+             JS_REG_SRV_VERSION, __DATE__, __TIME__ );
+
+    return g_sBuildInfo;
+}
 
 int REG_Service( JThreadInfo *pThInfo )
 {
@@ -86,8 +101,66 @@ int REG_SSL_Service( JThreadInfo *pThInfo )
     return 0;
 }
 
+int initServer()
+{
+    int ret = 0;
+    const char *value = NULL;
+
+    ret = JS_CFG_readConfig( g_sConfigPath, &g_pEnvList );
+    if( ret != 0 )
+    {
+        fprintf( stderr, "fail to open config file(%s)\n", g_sConfigPath );
+        exit(0);
+    }
+
+    value = JS_CFG_getValue( g_pEnvList, "DB_PATH" );
+    if( value == NULL )
+    {
+        fprintf( stderr, "You have to set 'DB_PATH'\n" );
+        exit(0);
+    }
+
+    g_dbPath = JS_strdup( value );
+
+    printf( "CA RegServer Init OK\n" );
+
+    return 0;
+}
+
+void printUsage()
+{
+    printf( "JS OCSP Server ( %s )\n", getBuildInfo() );
+    printf( "[Options]\n" );
+    printf( "-v         : Verbose on(%d)\n", g_nVerbose );
+    printf( "-c config : set config file(%s)\n", g_sConfigPath );
+    printf( "-h         : Print this message\n" );
+}
+
 int main( int argc, char *argv[] )
 {
+    int nOpt = 0;
+
+    sprintf( g_sConfigPath, "%s", "../ca_reg_srv.cfg" );
+
+    while(( nOpt = getopt( argc, argv, "c:vh")) != -1 )
+    {
+        switch( nOpt ) {
+        case 'h':
+            printUsage();
+            return 0;
+
+        case 'v':
+            g_nVerbose = 1;
+            break;
+
+        case 'c':
+            sprintf( g_sConfigPath, "%s", optarg );
+            break;
+        }
+    }
+
+    initServer();
+
     JS_THD_logInit( "./log", "reg", 2 );
     JS_THD_registerService( "JS_REG", NULL, 9030, 4, NULL, REG_Service );
     JS_THD_registerService( "JS_REG_SSL", NULL, 9130, 4, NULL, REG_SSL_Service );
--- ca_reg_srv/reg_srv.h
+++ ca_reg_srv/reg_srv.h
@@ -4,6 +4,8 @@
 #include "sqlite3.h"
 #include "js_bin.h"
 
+#define     JS_REG_SRV_VERSION          "0.9.1"
+
 int procReg( sqlite3 *db, const char *pReq, int nType, const char *pPath, char **ppRsp );
 
 #endif // REG_SRV_H
