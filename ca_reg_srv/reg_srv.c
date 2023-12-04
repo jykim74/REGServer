b9b560fba3365ac5433a6adf996a9c09aab3d717
jykim <jykim74@gmail.com>
2023-12-04 22:42:36 +0900

    오류 수정

--- ca_reg_srv/reg_srv.c
+++ ca_reg_srv/reg_srv.c
@@ -315,7 +315,7 @@ int main( int argc, char *argv[] )
 
     sprintf( g_sConfigPath, "%s", "../ca_reg_srv.cfg" );
 
-    while(( nOpt = getopt( argc, argv, "c:vh")) != -1 )
+    while(( nOpt = getopt( argc, argv, "c:d:vh")) != -1 )
     {
         switch( nOpt ) {
         case 'h':
